cbabeb9a810fad4f7418780f65b86a68f5766845
TheSereneZone <zoeysho@gmail.com>
2015-01-30 17:02:12 -0800

    Reorganize Code

---
+++ source/client/client.js
@@ -0,0 +1,24 @@
+if (Meteor.isClient) {
+  Template.body.helpers({
+    /*
+      Client-Side Template
+    */
+    lessons: [
+      { url: "https://www.youtube.com/embed/ScMzIvxBSi4", 
+        title: "Lesson One",
+        description: "Derpodile was here."
+      },
+      { url: "https://www.youtube.com/embed/XQu8TTBmGhA", 
+        title: "Lesson Two",
+        description: "Cher Ami is a bird."
+      }
+    ]
+  });
+
+  /*
+    Necessary for user login.
+  */
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+}
