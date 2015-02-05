6d17716494c680dff148e4f26311003f1ab18d9f
TheSereneZone <zoeysho@gmail.com>
2015-02-04 16:34:26 -0800

    Install Iron-Router

--- source/client/client.js
+++ source/client/client.js
@@ -22,3 +22,9 @@ if (Meteor.isClient) {
     passwordSignupFields: "USERNAME_ONLY"
   });
 }
+
+Router.route("/", function () {
+  this.render("Home", {
+    data: function () { return Items.findOne({_id: this.params._id}); }
+  });
+});
